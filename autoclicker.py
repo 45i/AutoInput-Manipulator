fff8907308d8aa60d96e4d28666b1ebd81ed35cc
PythaSnek <sayakpalit61@gmail.com>
2021-07-12 11:19:36 +0530

    Update autoclicker.py

--- autoclicker.py
+++ autoclicker.py
@@ -1,5 +1,13 @@
 import pyautogui
 import time
-a = int(input("Clicks: "))
-time.sleep(5)
-pyautogui.click(clicks=a, interval=0.2, button='left')
+
+e= Exception
+try:
+    a = int(input("Clicks: "))
+    b=input("Which Mouse Button?: (React with 'left' or 'right')")
+    b_split= b.lower().split()
+    time.sleep(5)
+    pyautogui.click(clicks=a, interval=0.2, button=b_split[0])
+except pyautogui.FailSafeException:
+    print("PyAutoGui triggered a FailSafe exception \n"
+          "Reason: Mouse moved to the corner of the screen")
